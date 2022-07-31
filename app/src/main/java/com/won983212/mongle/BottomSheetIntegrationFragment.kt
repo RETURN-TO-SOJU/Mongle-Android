23a4ec0369e32d002194dd7c3981efad39ce8190
이현 <won983212@naver.com>
2022-07-31 19:13:11 +0900

    fix: Cloud 및 Calendar 연동 BottomSheetDialog 추가

---
+++ app/src/main/java/com/won983212/mongle/BottomSheetIntegrationFragment.kt
@@ -0,0 +1,18 @@
+package com.won983212.mongle
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomSheetIntegrationFragment : BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.bottom_sheet_integration, container, false)
+    }
+}
