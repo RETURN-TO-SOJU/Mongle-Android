02904a1f40c41ad4cfcce127ee275b258f737c78
이현 <won983212@naver.com>
2022-07-31 14:19:39 +0900

    update: launcher icon 변경

--- app/src/main/java/com/won983212/mongle/LoginActivity.kt
+++ app/src/main/java/com/won983212/mongle/LoginActivity.kt
@@ -1,21 +1,38 @@
 package com.won983212.mongle
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.won983212.mongle.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     private var keepSplash: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
 
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        applySplashLazyOut(splashScreen)
+        attachEvents(binding)
+    }
+
+    private fun applySplashLazyOut(splashScreen: SplashScreen) {
         splashScreen.setKeepOnScreenCondition { keepSplash }
         Handler(Looper.getMainLooper())
             .postDelayed({ keepSplash = false }, 1000)
+    }
+
+    private fun attachEvents(binding: ActivityLoginBinding) {
+        binding.loginWithKakaoButton.setOnClickListener {
+            val intent = Intent(this, AgreeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
