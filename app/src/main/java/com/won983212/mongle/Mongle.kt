b80932df46dc2f6b9143bcfba28707f9fe18dc24
won983212 <won983212@naver.com>
2022-08-04 16:56:39 +0900

    feat: kakao login 토대 및 카카오 대화내용 공유 구현

---
+++ app/src/main/java/com/won983212/mongle/Mongle.kt
@@ -0,0 +1,16 @@
+package com.won983212.mongle
+
+import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+
+class Mongle : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Get key hash
+        // Log.i("KeyHash", Utility.getKeyHash(this))
+
+        // Initialize kakao sdk
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
+    }
+}
