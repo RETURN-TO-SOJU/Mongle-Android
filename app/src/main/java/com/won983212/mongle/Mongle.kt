870bb0c5138a8600acc2fe5385c29eb252640a4f
이현 <won983212@naver.com>
2022-08-09 21:05:43 +0900

    feat: 아이콘 버튼 ripple 개선

--- app/src/main/java/com/won983212/mongle/Mongle.kt
+++ app/src/main/java/com/won983212/mongle/Mongle.kt
@@ -1,14 +1,16 @@
 package com.won983212.mongle
 
 import android.app.Application
+import android.util.Log
 import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.Utility
 
 class Mongle : Application() {
     override fun onCreate() {
         super.onCreate()
 
         // Get key hash
-        // Log.i("KeyHash", Utility.getKeyHash(this))
+        Log.i("KeyHash", Utility.getKeyHash(this))
 
         // Initialize kakao sdk
         KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
