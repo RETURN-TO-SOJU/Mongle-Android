2b8fd85b0d15e21b9552c5f5dd5b6fbeba5c8c4b
won983212 <won983212@naver.com>
2022-09-30 17:43:51 +0900

    feat: tutorial - subtitle 사용시 ui 디자인 개선

--- app/src/main/java/com/won983212/mongle/presentation/view/tutorial/TutorialPageFragment.kt
+++ app/src/main/java/com/won983212/mongle/presentation/view/tutorial/TutorialPageFragment.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.core.content.res.ResourcesCompat
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import com.won983212.mongle.R
-import com.won983212.mongle.databinding.FragmentTutorialPageBinding
 import com.won983212.mongle.presentation.util.dpToPxInt
 
 class TutorialPageFragment : Fragment() {
@@ -34,25 +34,36 @@ class TutorialPageFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        val useSubtitle = isValidRes(subtitleRes)
         val context = requireContext()
-        val view = FragmentTutorialPageBinding.inflate(inflater, container, false)
+
+        val view = if (useSubtitle) {
+            inflater.inflate(R.layout.fragment_tutorial_page_subtitle, container, false)
+        } else {
+            inflater.inflate(R.layout.fragment_tutorial_page, container, false)
+        }
+
         view.apply {
-            if (isValidRes(subtitleRes)) {
-                textTutorialTitle.textSize = 20f
-                textTutorialTitle.typeface = ResourcesCompat.getFont(context, R.font.app_bold)
+            val textTutorialTitle = findViewById<TextView>(R.id.text_tutorial_title)
+            val imageTutorial = findViewById<ImageView>(R.id.image_tutorial)
+
+            if (useSubtitle) {
+                val textTutorialSubtitle = findViewById<TextView>(R.id.text_tutorial_subtitle)
                 textTutorialSubtitle.text = context.getText(subtitleRes!!)
-                textTutorialSubtitle.visibility = View.VISIBLE
             }
+
             if (isValidRes(titleRes) && isValidRes(imageRes)) {
                 textTutorialTitle.text = context.getText(titleRes!!)
                 imageTutorial.setImageResource(imageRes!!)
             }
+
             if (imageWidth != null && imageWidth != 0f) {
                 imageTutorial.layoutParams.width = dpToPxInt(context, imageWidth!!.toInt())
                 imageTutorial.requestLayout()
             }
         }
-        return view.root
+
+        return view
     }
 
     private fun isValidRes(res: Int?): Boolean {
