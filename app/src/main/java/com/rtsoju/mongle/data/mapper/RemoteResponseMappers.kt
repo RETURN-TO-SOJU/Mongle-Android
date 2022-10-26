b58d15487c567a30e2b973b8287f29f63f7c15bd
won983212 <won983212@naver.com>
2022-10-26 20:49:16 +0900

    feat: Statistics Domain모델 생성 및 mapper구현

--- app/src/main/java/com/rtsoju/mongle/data/mapper/RemoteResponseMappers.kt
+++ app/src/main/java/com/rtsoju/mongle/data/mapper/RemoteResponseMappers.kt
@@ -3,6 +3,7 @@ package com.rtsoju.mongle.data.mapper
 import com.rtsoju.mongle.data.source.remote.dto.response.*
 import com.rtsoju.mongle.domain.model.*
 import java.time.LocalDate
+import java.time.Period
 
 fun CalendarDayDetailResponse.toDomainModel(date: LocalDate): CalendarDayDetail {
     return CalendarDayDetail(
@@ -45,8 +46,53 @@ fun LoginResponse.toDomainModel(): LoginResult {
         refreshTokenExpiresAt,
         isNew
     )
+
 }
 
 fun UserResponse.toDomainModel(): User {
     return User(username, kakaoName)
+}
+
+
+private fun <T> packStatisticsEmotions(statisticsData: StatisticsResponse.Data<T>): List<Int> {
+    val result = IntArray(Emotion.values().size)
+    result[Emotion.HAPPY.ordinal] = statisticsData.happy
+    result[Emotion.ANGRY.ordinal] = statisticsData.angry
+    result[Emotion.ANXIOUS.ordinal] = statisticsData.anxious
+    result[Emotion.SAD.ordinal] = statisticsData.sad
+    result[Emotion.NEUTRAL.ordinal] = statisticsData.neutral
+    result[Emotion.TIRED.ordinal] = statisticsData.tired
+    return result.asList()
+}
+
+fun StatisticsResponse<Float?>.toYearlyDomainModel(): Statistics {
+    return Statistics(
+        data.scoreList.mapIndexed { idx, score -> Statistics.Score("${idx + 1}월", score) },
+        packStatisticsEmotions(data)
+    )
+}
+
+fun StatisticsResponse<Float?>.toMonthlyDomainModel(): Statistics {
+    return Statistics(
+        data.scoreList.mapIndexed { idx, score -> Statistics.Score("${idx + 1}주", score) },
+        packStatisticsEmotions(data)
+    )
+}
+
+fun StatisticsResponse<StatisticsResponse.DateScore>.toWeeklyDomainModel(): Statistics {
+    val startDate = data.startDate
+    val scores = Array<Float?>(7) { null }
+
+    data.scoreList.map {
+        val idx = Period.between(startDate, it.date).days
+        scores[idx] = it.score
+    }
+
+    return Statistics(
+        scores.mapIndexed { idx, score ->
+            val day = startDate.plusDays(idx.toLong())
+            Statistics.Score("${day.dayOfMonth}일", score)
+        },
+        packStatisticsEmotions(data)
+    )
 }
---
+++ app/src/main/java/com/rtsoju/mongle/domain/model/Statistics.kt
@@ -0,0 +1,13 @@
+package com.rtsoju.mongle.domain.model
+
+data class Statistics(
+    /** 행복 점수를 나타냄. 주별이면 7개, 월별이면 그 월의 week개, 연별이면 12개 */
+    val scores: List<Score>,
+    /** 감정별 문장 총 개수. Emotion.ordinary 위치에 그 감정 개수가 저장됨 */
+    val emotionCount: List<Int>
+) {
+    data class Score(
+        val label: String,
+        val score: Float?
+    );
+}
