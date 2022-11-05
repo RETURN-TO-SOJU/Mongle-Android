d5592a4228fec39556b4e02c9ca044d4b795b9b9
won983212 <won983212@naver.com>
2022-11-05 21:17:07 +0900

    feat: 강제 업데이트 UI
    Resolves: #36

--- app/src/main/java/com/rtsoju/mongle/presentation/view/starting/StartingActivity.kt
+++ app/src/main/java/com/rtsoju/mongle/presentation/view/starting/StartingActivity.kt
@@ -2,10 +2,15 @@ package com.rtsoju.mongle.presentation.view.starting
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.rtsoju.mongle.R
 import com.rtsoju.mongle.USE_TEST_ACTIVITY
 import com.rtsoju.mongle.debug.view.MainTestActivity
@@ -19,6 +24,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class StartingActivity : BaseActivity() {
     private val viewModel by viewModels<StartingViewModel>()
     private lateinit var loginFlow: LoginFlow
+    private lateinit var appUpdateManager: AppUpdateManager
 
     private fun makePasswordScreenLauncher(): ActivityResultLauncher<Intent> {
         return registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
@@ -26,6 +32,33 @@ class StartingActivity : BaseActivity() {
                 loginFlow.launch()
             } else {
                 finish()
+            }
+        }
+    }
+
+    private fun makeLoginFlow() {
+        loginFlow = LoginFlow(this) {
+            if (it == LoginFlow.LoginResult.REGISTER || it == LoginFlow.LoginResult.LOGIN) {
+                Intent(applicationContext, MainActivity::class.java).apply {
+                    putExtra(MainActivity.EXTRA_SHOW_SHOWCASE, it == LoginFlow.LoginResult.REGISTER)
+                    startActivity(this)
+                }
+            }
+            finish()
+        }
+    }
+
+    private fun checkUpdate() {
+        appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+            ) {
+                appUpdateManager.startUpdateFlowForResult(
+                    appUpdateInfo,
+                    AppUpdateType.IMMEDIATE,
+                    this,
+                    REQUEST_APP_UPDATE
+                )
             }
         }
     }
@@ -42,15 +75,9 @@ class StartingActivity : BaseActivity() {
             return
         }
 
-        loginFlow = LoginFlow(this) {
-            if (it == LoginFlow.LoginResult.REGISTER || it == LoginFlow.LoginResult.LOGIN) {
-                Intent(applicationContext, MainActivity::class.java).apply {
-                    putExtra(MainActivity.EXTRA_SHOW_SHOWCASE, it == LoginFlow.LoginResult.REGISTER)
-                    startActivity(this)
-                }
-            }
-            finish()
-        }
+        appUpdateManager = AppUpdateManagerFactory.create(this)
+        checkUpdate()
+        makeLoginFlow()
 
         if (viewModel.needsPasswordAuth()) {
             makePasswordScreenLauncher().launch(
@@ -61,5 +88,33 @@ class StartingActivity : BaseActivity() {
         } else {
             loginFlow.launch()
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+                appUpdateManager.startUpdateFlowForResult(
+                    appUpdateInfo,
+                    AppUpdateType.IMMEDIATE,
+                    this,
+                    REQUEST_APP_UPDATE
+                )
+            }
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_APP_UPDATE) {
+            if (resultCode != RESULT_OK) {
+                Log.e("StartingActivity", "Update flow failed! Result code: $resultCode")
+            }
+        }
+    }
+
+    companion object {
+        private const val REQUEST_APP_UPDATE = 101
     }
 }
